89cdf056d4fadfacec4b7145e4b301789c83cefc
ortiqovdev <akmalbekortiqov7976@gmail.com>
2022-12-22 12:19:51 +0500

    Boolean 1-12

---
+++ boolean.js
@@ -0,0 +1,93 @@
+export{}
+// Mantiqiy amallarga oid masalalar
+
+// boolean1
+// let a = 10
+// console.log(a > 0 ? 'Musbat ' : "manfiy");
+
+// boolean2
+// let a = 3
+// console.log(a % 2===1  ? 'toq' : "juft");
+
+// boolean3
+// let a = 8
+// console.log(a % 2===1  ? 'toq' : "juft");
+
+// boolean4
+// let a=2
+// let b=3
+// console.log(a>2 && b<=3  ? 'rost' : 'xato')
+
+
+// boolean5
+// let a=10
+// let b=11
+// console.log(a>=2 && b<=-2 ? 'togri' : 'xato')
+
+
+// boolean6
+// let a=10
+// let b=11
+// let c=12
+// console.log(a<=b<=c? 'togri' : 'xato')
+
+
+// boolean7
+// let a=10
+// let b= 15
+// let c=20
+// console.log(a<b && b<c);
+
+// boolean8
+// let a=11
+// let b=21
+// console.log(a%2==1 && b%2==1);
+
+// boolean9
+// let a=13
+// let b=11
+// console.log(a%2==1 || b%2==1 );
+
+// boolean10
+// let a=22
+// let b=21
+// console.log((a%2==1 && b%2==0) || (a%2==0 && b%2==1));
+
+// boolean11
+// let a = 11
+// let b=21
+// console.log(a%2==0 && b%2==0  || a%2==1 && b%2==1 ? 'rost' : 'xato');
+
+// boolean12
+let a=12
+let b=14
+let c=13
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// boolean 12345678910111213141516517181920
+// let a = 45758;
+// console.log(a > 0 ? (a % 2 == 0 ? "Musbat Juft" : "Musbat Toq") : (a % 2 == -1 ? "Manfiy Toq" : "Manfiy Juft"));
