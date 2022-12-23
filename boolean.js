b00487edc14603c7e13b965276aacea5c938da7b
Akmal Ortiqov <116548309+ortiqovdev@users.noreply.github.com>
2022-12-23 23:48:27 +0500

    boolean 1-30

--- boolean.js
+++ boolean.js
@@ -59,35 +59,125 @@ export{}
 // console.log(a%2==0 && b%2==0  || a%2==1 && b%2==1 ? 'rost' : 'xato');
 
 // boolean12
-let a=12
-let b=14
-let c=13
-console.log();
+// let a=10
+// let b=20
+// let c=30
+// console.log(a>0 && b>0 && c>0 ? 'rost' : 'yolgon' );
+
+// boolean13
+// let a=-10
+// let b=-20
+// let c=30
+// console.log(a<0 && b<0 || c<0 && c>0 ? 'rost' : 'yolgon' );
+
+// boolean14
+// let a=-5
+// let b=-10
+// let c=15
+// console.log(a<0 && b<0 || c<0  ? 'rost' : 'yolgon' );
+
+// boolean15
+// let a=12
+// let b=15
+// let c=-20
+// console.log(a>0 && b>0 && c<0 ? 'rost' : 'yolgon');
+
+//boolean16
+// let a=78
+// console.log(  a%2==0 && 9<a && a<100 ? 'rost ' : 'yolgon' ) ; 
+
+// boolean17
+// let a=999
+// console.log(  a%2==1 && 99<a && a<1000 ? 'rost ' : 'yolgon' ) ; 
+
+// boolean18
+// let a=20
+// let b=21
+// let c=20
+// console.log(b==c || c==a || a==b ? 'rost' : 'yolgon');
+
+// boolean19
+// let a= 23
+// let b=20
+// let c=23
+// console.log( (a!==b || a==b) && (b==c || a==c)? 'rost' :'yolgon' );
+
+// boolean20
+// let a=12
+// let b=145
+// let c=14
+// console.log(a!==b && b!==c && c!==a ? 'rost': 'yolgon');
+
+// boolean21
+// let n=167
+// let a=Math.floor(n%10)
+// let b= Math.floor(n/10)
+// let c=Math.ceil(b%10)
+// let d=Math.floor(n/100)
+// console.log( d<c && c<a  ? 'rost' : 'yolgon');
+
+// boolean22
+// let n=123
+// let a=Math.floor(n%10)
+// let b= Math.floor(n/10)
+// let c=Math.ceil(b%10)
+// let d=Math.floor(n/100)
+// console.log( (d<c && c<a) || (a<c && c<b) ? 'rost' : 'yolgon');
+
+//* boolean23
+// let n=191
+// let a=Math.floor(n%10)
+// let b= Math.floor(n/10)
+// let c=Math.ceil(b%10)
+// let d=Math.floor(n/100)
+// console.log( d<c && c>a  ? 'rost' : 'yolgon');
+
+//* boolean24
+// let a=10
+// let b=15
+// let c=5
+// let d=(b*b)-(4*a*c)
+// console.log(d>=0 ? 'rost':'yolgon');
+
+//* boolean25
+// let x=-10
+// let y=15
+// console.log(x<0 && y>0 ? 'rost' :'yolgon');
+
+// * boolaen26
+// let x=10
+// let y=-15
+// console.log(x>0 && y<0 ? 'rost' : 'yolgon'); 
+
+// * boolean27
+// let x=-20
+// let y=10
+// console.log(x<0 && (y>0 || y<0) ? 'rost': 'yolgon');
+
+// * boolean28
+// let x=-10
+// let y=-15
+// console.log(x>0 && y>0 || x<0 && y<0 ? 'rost' :'yolgon');
+
+// ! boolean29
+// let x=10
+// let y=15
+// let x1= 5
+// let y1 =7
+// let x2 = 20
+// let y2 =22
+// console.log();
+
+// *boolean30
+// let a=30
+// let b=30
+// let c=30
+// console.log(a==b && b==c ? 'rost':'yolgon');
+  
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// boolean 12345678910111213141516517181920
+// boolean 123456789
 // let a = 45758;
 // console.log(a > 0 ? (a % 2 == 0 ? "Musbat Juft" : "Musbat Toq") : (a % 2 == -1 ? "Manfiy Toq" : "Manfiy Juft"));
